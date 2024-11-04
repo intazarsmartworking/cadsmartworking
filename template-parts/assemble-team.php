46fe471bffcb640ac8f041ac4bf863719de0dc2c
intazar <intazar@smartworking.io>
2024-11-04 20:01:11 +0530

    working on file

--- template-parts/assemble-team.php
+++ template-parts/assemble-team.php
@@ -81,27 +81,11 @@
                      <div class="rounded-2xl p-3 lg:px-[2.5rem] border-pattern ">
                         <h6 class="text-dark-orange font-bold text-2xl"> Share your requirements </h6>
 
-                        <!--<form method="post">
-
-                        <input type="text" id="skillsInput"  name="skills"  readonly
-                        class="mt-4 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-dark-black focus:border-dark-black  sm:text-sm">
-
-                            <input type="text"  name="fullname" placeholder="Full Name" 
-                            class="mt-4 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-dark-black focus:border-dark-black  sm:text-sm">
-
-
-                            <input type="email"  name="email-address" placeholder="Email Address" 
-                            class="mt-4 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-dark-black focus:border-dark-black  sm:text-sm">
-                            
-                            <button type="submit" class="w-full bg-dark-orange text-white mt-4 py-2 px-4 rounded-md hover:bg-dark-black focus:outline-none focus:bg-dark-black transition duration-200">Submit Now</button>
-                        </form>-->
+                        
 
                         <?php echo do_shortcode('[contact-form-7 id="713a21a" title="Assemble Team Form"]'); ?>
                 </div>
-                    <!-- <div class="rounded-2xl border-pattern ">
-                        <iframe class="home-form" aria-label='Share Your Requirement' frameborder="0" style="height:330px;width:100%;border:none;" src='https://forms.zohopublic.in/shubhankit/form/SkillPagesForm/formperma/Kg0mSBrC00V3ydnzP2AdZWY3HYv-yjP5y0vJV2CTa3E'></iframe>
-                        <h4 class="text-center text-dark-black text-sm font-semibold pt-3"> Our recruitment specialist will be in touch soon! </h4>
-                    </div>  -->
+                    
                     
                     
                     <div class="rounded-2xl px-6 pt-6 pb-3  lg:px-[2.5rem] border-pattern mt-4 hide-mobile">
